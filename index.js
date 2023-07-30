38e002d0879c6e53b2ba02f5c134c6767db1f588
infect01 <140965793+infect01@users.noreply.github.com>
2023-07-30 23:35:55 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const EncoderDecoder = require('./utils.js');
-const fingerprint = require('./fingerprint.js');
-
-const encoderDecoder = new EncoderDecoder();
-
-const encodedXal = encoderDecoder.encode(fingerprint);
-const decodedXal = encoderDecoder.decode(encodedXal);
